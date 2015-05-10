1727132e156cbd3a26d78fb5f8bcdf3457667629
Hernandes B. de Sousa <hernandes.sousa@gmail.com>
2015-05-09 22:27:14 -0300

    Add AlertDialog for error messages

---
+++ app/src/main/java/me/hernandes/stormy/AlertDialogFragment.java
@@ -0,0 +1,22 @@
+package me.hernandes.stormy;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * Created by hernandes on 09/05/15.
+ */
+public class AlertDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .setTitle(context.getString(R.string.error_title))
+                .setMessage(context.getString(R.string.error_message))
+                .setPositiveButton(context.getString(R.string.error_ok_button_text), null);
+        return builder.create();
+    }
+}
