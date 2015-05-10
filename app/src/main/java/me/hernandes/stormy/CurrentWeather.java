1f27f7f36c6056cc826bdb68080b9724cc319019
Hernandes B. de Sousa <hernandes.sousa@gmail.com>
2015-05-09 22:29:39 -0300

    Add CurrentWeather model class

---
+++ app/src/main/java/me/hernandes/stormy/CurrentWeather.java
@@ -0,0 +1,58 @@
+package me.hernandes.stormy;
+
+/**
+ * Created by hernandes on 09/05/15.
+ */
+public class CurrentWeather {
+    private String mIcon, mSummary;
+    private long mTime;
+    private double mTemperature, mHumidity, mPrecipChance;
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public void setTime(long time) {
+        mTime = time;
+    }
+
+    public double getTemperature() {
+        return mTemperature;
+    }
+
+    public void setTemperature(double temperature) {
+        mTemperature = temperature;
+    }
+
+    public double getHumidity() {
+        return mHumidity;
+    }
+
+    public void setHumidity(double humidity) {
+        mHumidity = humidity;
+    }
+
+    public double getPrecipChance() {
+        return mPrecipChance;
+    }
+
+    public void setPrecipChance(double precipChance) {
+        mPrecipChance = precipChance;
+    }
+}
