fd6d6371a1778173c054c1085608f34749598cec
Hernandes B. de Sousa <hernandes.sousa@gmail.com>
2015-08-10 23:10:57 -0300

    Refactor to use Forecast class on Main Activity
    This is so we can better work with te List of Hourly weather forecasts.

--- app/src/main/java/me/hernandes/stormy/ui/MainActivity.java
+++ app/src/main/java/me/hernandes/stormy/ui/MainActivity.java
@@ -28,12 +28,13 @@ import butterknife.ButterKnife;
 import butterknife.InjectView;
 import me.hernandes.stormy.R;
 import me.hernandes.stormy.weather.Current;
+import me.hernandes.stormy.weather.Forecast;
 
 
 public class MainActivity extends ActionBarActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
-    private Current mCurrent;
+    private Forecast mForecast;
 
     @InjectView(R.id.timeLabel) TextView mTimeLabel;
     @InjectView(R.id.temperatureLabel) TextView mTemperatureLabel;
@@ -103,7 +104,7 @@ public class MainActivity extends ActionBarActivity {
                         String jsonData = response.body().string();
                         Log.v(TAG, jsonData);
                         if (response.isSuccessful()) {
-                            mCurrent = getCurrentDetails(jsonData);
+                            mForecast = parseForecastDetails(jsonData);
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
@@ -137,14 +138,21 @@ public class MainActivity extends ActionBarActivity {
     }
 
     private void updateDisplay() {
-        mTemperatureLabel.setText(mCurrent.getTemperature() + "");
-        mTimeLabel.setText("At " + mCurrent.getFormattedTime() + " it will be");
-        mHumidityValue.setText(mCurrent.getHumidity() + "");
-        mPrecipValue.setText(mCurrent.getPrecipChance() + "%");
-        mSummaryLabel.setText(mCurrent.getSummary());
+        Current current = mForecast.getCurrent();
+        mTemperatureLabel.setText(current.getTemperature() + "");
+        mTimeLabel.setText("At " + current.getFormattedTime() + " it will be");
+        mHumidityValue.setText(current.getHumidity() + "");
+        mPrecipValue.setText(current.getPrecipChance() + "%");
+        mSummaryLabel.setText(current.getSummary());
 
-        Drawable drawable = getResources().getDrawable(mCurrent.getIconId());
+        Drawable drawable = getResources().getDrawable(current.getIconId());
         mIconImageView.setImageDrawable(drawable);
+    }
+
+    private Forecast parseForecastDetails(String jsonData) throws JSONException {
+        Forecast forecast = new Forecast();
+        forecast.setCurrent(getCurrentDetails(jsonData));
+        return forecast;
     }
 
     private Current getCurrentDetails(String jsonData) throws JSONException{
@@ -160,10 +168,10 @@ public class MainActivity extends ActionBarActivity {
         double humidity = currently.getDouble("humidity");
         double precipChance = currently.getDouble("precipProbability");
 
-        mCurrent = new Current(icon, summary, time, temperature, humidity, precipChance, timezone);
-        Log.d(TAG, mCurrent.getFormattedTime());
+        Current current = new Current(icon, summary, time, temperature, humidity, precipChance, timezone);
+        Log.d(TAG, current.getFormattedTime());
 
-        return mCurrent;
+        return current;
     }
 
     private boolean isNetworkAvailable() {
